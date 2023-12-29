3ad9e84bcda3f5a69775fefb978cfedbc6e86091
narrowgap <101340280+narrowgap@users.noreply.github.com>
2023-12-29 19:39:22 +0700

    Add files via upload

--- src/hidden_singles.c
+++ src/hidden_singles.c
@@ -1,6 +1,83 @@
 #include "hidden_singles.h"
+#include <stdbool.h>
 
-int hidden_singles(SudokuBoard *p_board)
-{
-    return 0;
+#define NULL ((void*)0)
+
+typedef struct {
+    int row;
+    int col;
+    int value;
+} HiddenSingles;
+
+void find_hidden_single(Cell** p_cells, HiddenSingles* p_hidden_singles, int* p_counter) {
+    for (int value = 1; value <= BOARD_SIZE; ++value) {
+        int candidate_count = 0;
+        Cell* candidate_cell = NULL;
+
+        for (int i = 0; i < BOARD_SIZE; ++i) {
+            Cell* cell = p_cells[i];
+
+            // Skip cells that are already solved
+            if (cell->num_candidates == 1) {
+                continue;
+            }
+
+            // Check if the value is a candidate in the cell
+            if (cell->candidates[value - 1] != 0) {
+                candidate_count++;
+                candidate_cell = cell;
+            }
+        }
+
+        // If only one cell has the value as a candidate, it's a hidden single
+        if (candidate_count == 1) {
+            HiddenSingles hidden_single = {
+                .row = candidate_cell->row_index,
+                .col = candidate_cell->col_index,
+                .value = value
+            };
+
+            // Store the information about the hidden single cell
+            bool new_hidden_single = true;
+            for (int i = 0; i < *p_counter; ++i) {
+                if (p_hidden_singles[i].row == hidden_single.row &&
+                    p_hidden_singles[i].col == hidden_single.col) {
+                    new_hidden_single = false;
+                    break;
+                }
+            }
+
+            if (new_hidden_single) {
+                p_hidden_singles[*p_counter] = hidden_single;
+                (*p_counter)++;
+            }
+        }
+    }
+}
+
+int hidden_singles(SudokuBoard* p_board) {
+    int total_hidden_singles = 0;
+    HiddenSingles hidden_cell[BOARD_SIZE * BOARD_SIZE];
+
+    // Iterate through rows, columns, and boxes to check hidden singles
+    for (int i = 0; i < BOARD_SIZE; ++i) {
+        find_hidden_single(p_board->p_rows[i], hidden_cell, &total_hidden_singles);
+        find_hidden_single(p_board->p_cols[i], hidden_cell, &total_hidden_singles);
+        find_hidden_single(p_board->p_boxes[i], hidden_cell, &total_hidden_singles);
+    }
+
+    // Update the Sudoku board based on hidden singles
+    for (int i = 0; i < total_hidden_singles; ++i) {
+        for (int value = 1; value <= 9; ++value) {
+            if (value != hidden_cell[i].value) {
+                Cell* cell = &p_board->data[hidden_cell[i].row][hidden_cell[i].col];
+                if (cell->candidates[value - 1] != 0) {
+                    cell->candidates[value - 1] = 0;
+                    cell->num_candidates--;
+                }
+            }
+        }
+    }
+
+    return total_hidden_singles;
 }
--- src/hidden_triples.c
+++ src/hidden_triples.c
@@ -1,6 +1,53 @@
 #include "hidden_triples.h"
 
-int hidden_triples(SudokuBoard *p_board)
-{
-    return 0;
+// Function to check if an array of cells contains a hidden triple
+bool is_triple(Cell **p_cells, int size) {
+    int candidate = p_cells[0]->value;
+    int triple_count = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (p_cells[i]->value == candidate) {
+            triple_count++;
+        }
+    }
+
+    return triple_count == 3;
+}
+
+// Function to find and eliminate hidden triples in a Sudoku board
+int hidden_triples(SudokuBoard *p_board) {
+    int changed_cells = 0;
+
+    // Iterate over each 3x3 block in the Sudoku board
+    for (int row = 0; row < BOARD_SIZE; row++) {
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            Cell *p_cells[BOARD_SIZE * BOARD_SIZE];
+            int size = 0;
+
+            // Collect cells in the current 3x3 block into an array
+            for (int r = row; r < row + 3 && r < BOARD_SIZE; r++) {
+                for (int c = col; c < col + 3 && c < BOARD_SIZE; c++) {
+                    p_cells[size++] = &p_board->data[r][c];
+                }
+            }
+
+            // Check if the collected cells form a hidden triple
+            if (is_triple(p_cells, size)) {
+                int candidate = p_cells[0]->value;
+
+                // Remove the candidate from other cells in the block
+                for (int i = 0; i < size; i++) {
+                    if (p_cells[i]->value != candidate) {
+                        // Assuming remove_candidate is a function to eliminate a candidate
+                        if (remove_candidate(p_cells[i], candidate)) {
+                            changed_cells++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Return the total number of changed cells
+    return changed_cells;
 }
